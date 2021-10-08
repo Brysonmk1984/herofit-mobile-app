f03038621a0c4d658eb924e606d6e1a2adeeabfe
Bryson Kruk <brysonmk1984@gmail.com>
2021-10-08 12:12:15 -0600

    fixed bug with popping back

--- App/Screens/Battle/AwaitingBattle/AwaitingBattle.tsx
+++ App/Screens/Battle/AwaitingBattle/AwaitingBattle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
 import { MainDrawerProps } from "../../../common/types-navigator";
 import { ScreenContainer } from "../../../Components/CustomComponents";
@@ -14,11 +14,19 @@ const AwaitingBattle: React.FC<MainDrawerProps<"AwaitingBattle">> = ({ navigatio
   // On item click, open Item modal
   // Run Battle now button when applicable
 
+  useEffect(() => {
+    const screenPop = setTimeout(() => {
+      navigation.pop();
+    }, 3000);
+
+    return () => clearTimeout(screenPop);
+  }, []);
+
   return (
     <ScreenContainer screenName={route.name}>
       <HeroSection rewards={rewards} height={height} width={width} />
       <FoeSection foe={foe} height={height} width={width} character={character} />
-      <VsSection height={height} navigation={navigation} />
+      <VsSection height={height} pop={navigation.pop} />
     </ScreenContainer>
   );
 };
--- App/Screens/Battle/AwaitingBattle/VsSection.tsx
+++ App/Screens/Battle/AwaitingBattle/VsSection.tsx
@@ -1,32 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { VStack, Text, Pressable, Box } from "native-base";
 import { CountdownTimer } from "../../Home/Components/TopHud/CountdownTimer";
 import { LinearGradient } from "expo-linear-gradient";
 import herofitTheme from "../../../styles/herofitTheme";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { MainDrawerParamList } from "../../../common/types-navigator";
 
 interface VsSectionProps {
   height: number;
-  navigation: StackNavigationProp<MainDrawerParamList, "AwaitingBattle">;
+  pop: (count?: number) => void;
 }
 
-const VsSection: React.FC<VsSectionProps> = ({ height, navigation }) => {
+const VsSection: React.FC<VsSectionProps> = ({ height, pop }) => {
   const vsColors = ["transparent", "#ffffff", "transparent"];
-
-  useEffect(() => {
-    const screenPop = setTimeout(() => {
-      navigation.pop();
-    }, 6000);
-
-    return clearTimeout(screenPop);
-  }, []);
 
   return (
     <VStack zIndex={100} h={height * 0.45} w="100%" justifyContent="center" alignItems="center" position="absolute" top={height * 0.26}>
       <LinearGradient colors={vsColors} style={styles.vsGradient} alignItems="center" />
-      <Pressable onPress={() => navigation.pop()}>
+      <Pressable onPress={() => pop()}>
         <Box justifyContent="center" alignItems="center">
           <Text style={styles.textShadow} mt={-10} mb={-5} fontSize={140} fontFamily="heading">
             VS
