3e5e95b817b7168ec6e5024b593651ac49a40a7b
Bryson Kruk <brysonmk1984@gmail.com>
2021-11-24 16:08:54 -0700

    added sentrygit status

--- App/App.tsx
+++ App/App.tsx
@@ -13,6 +13,7 @@ import { getHeroAlias } from "./common/helperFunctions";
 import { View } from "native-base";
 import useGlobalToast from "./common/hooks/useGlobalToast";
 import { Logs } from "expo";
+
 import * as Linking from "expo-linking";
 import Constants from "expo-constants";
 LogBox.ignoreLogs(["Reanimated 2", "Remote debugger", "VirtualizedLists should never be nested", 'Expected style "lineHeight: 24" to contain units', 'Expected style "lineHeight: 30" to contain units', 'Expected style "lineHeight: 40" to contain units', 'Expected style "lineHeight: 50" to contain units', 'Expected style "lineHeight: 85" to contain units', 'Expected style "lineHeight: 120" to contain units', "Please pass alt prop to Image component", "Non-serializable values were found in the navigation state", "When server rendering, you must wrap your application in an <SSRProvider> to ensure consistent ids are generated between the client and server.", "VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead."]);
@@ -112,6 +113,7 @@ const App: React.FC = () => {
   //     background: "#000",
   //   },
   // };
+
   return (
     <NavigationContainer /* theme={MyTheme} linking={linking}*/>
       <View style={{ height }}>
--- App/index.tsx
+++ App/index.tsx
@@ -5,6 +5,19 @@ import { StateProvider } from "./store";
 import App from "./App";
 import herofitTheme from "./styles/herofitTheme";
 import "react-native-gesture-handler";
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://051e343d956b42e3bb7286f93ffe965d@o1077292.ingest.sentry.io/6080100",
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
+
+// Access any @sentry/react-native exports via:
+//Sentry.Native.*;
+
+// Access any @sentry/browser exports via:
+//Sentry.Browser.*;
 
 const Root = () => {
   const theme = extendTheme(herofitTheme);
