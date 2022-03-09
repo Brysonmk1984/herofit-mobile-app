b37153c53579ed2310db35b3b6f036512aabbe87
Bryson Kruk <brysonmk1984@gmail.com>
2022-03-09 12:10:29 -0700

    configuration for testing

--- __tests__/firstTest.test.tsx
+++ __tests__/firstTest.test.tsx
@@ -1,5 +1,5 @@
 describe("Misk test tests", () => {
   it("Should equal 1", () => {
-    expect(1).toBe(1);
+    expect(1).toBe(2);
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,31 @@ module.exports = {
   preset: 'jest-expo',
   setupFiles: [
     './node_modules/react-native-gesture-handler/jestSetup.js',
-
+    './__tests__/mocks/nativeModules.ts',
+    './__tests__/mocks/async-storage.ts'
   ],
   setupFilesAfterEnv: [
     "./__tests__/setup/nullOrAny.setup.tsx",
     "./__tests__/setup/jestFetchMockEnabled.setup.tsx"
   ],
+
+  /* THIS IS NEEDED TO RUN TESTS */
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json', // as specified by ts-jest
+      babelConfig: true,
+    },
+  },
+  //transformIgnorePatterns: [],
+  /* END - THIS IS NEEDED TO RUN TESTS */
+
+  /* OLD SETTING BEFORE ABOVE CODE */
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+    "node_modules/(?!(jest-)?@react-native|react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
   ],
+/* END - OLD SETTING BEFORE ABOVE CODE */
+
+
   "testPathIgnorePatterns": [
     "./__tests__/setup"
   ],
@@ -23,3 +39,5 @@ module.exports = {
     "json"
   ],
 };
+
+
