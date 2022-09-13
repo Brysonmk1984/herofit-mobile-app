b90f6684d75b1d06380c54abb5f60f288bbac52a
Bryson Kruk <brysonmk1984@gmail.com>
2022-09-13 13:28:06 -0600

    fixed button color in sidebar, added donate button

--- App/Screens/Home/Components/SidebarMenu.tsx
+++ App/Screens/Home/Components/SidebarMenu.tsx
@@ -21,7 +21,12 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ navigation, setSideDrawerOpen
   return (
     <VStack bgColor="base.primary" height="100%" justifyContent="space-between" borderLeftWidth="1" borderLeftColor="base.brand" pb={5}>
       <VStack space={1} pt={5}>
-        <Button variant="ghost" borderColor="#40403e" borderBottomWidth="1" borderTopWidth="1" pl={3} mt={5} onPress={() => handleNavigation(() => navigation.push("Adversaries"))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
+        <Button variant="ghost" borderColor="#40403e" borderTopWidth="1" borderBottomWidth="1" pl={3} mt={5} onPress={() => handleNavigation(() => WebBrowser.openBrowserAsync(`https://herofitgame.com/#donate`))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
+          <Text color="base.highlight" fontSize="2xl" fontFamily="bebas-neue">
+            Donate
+          </Text>
+        </Button>
+        <Button variant="ghost" borderColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => navigation.push("Adversaries"))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
           Adversaries
         </Button>
         <Button variant="ghost" borderBottomColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => navigation.push("Feedback"))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
@@ -36,9 +41,9 @@ const SidebarMenu: React.FC<SidebarMenuProps> = ({ navigation, setSideDrawerOpen
         <Button variant="ghost" borderBottomColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => WebBrowser.openBrowserAsync(`https://herofit.io/ranking/`))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
           Ranking
         </Button>
-        <Button variant="ghost" borderBottomColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => WebBrowser.openBrowserAsync(`https://herofit.io/users/${encodeURI(heroName)}`))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
+        {/* <Button variant="ghost" borderBottomColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => WebBrowser.openBrowserAsync(`https://herofit.io/users/${encodeURI(heroName)}`))} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
           Profile
-        </Button>
+        </Button> */}
         <Button variant="ghost" borderBottomColor="#40403e" borderBottomWidth="1" pl={3} onPress={() => handleNavigation(() => signOut())} fontSize="2xl" fontFamily="heading" _text={{ color: "primary.200" }} _pressed={{ _text: { color: "base.brand" }, bgColor: "base.primary" }}>
           Sign Out
         </Button>
--- App/styles/herofitTheme.ts
+++ App/styles/herofitTheme.ts
@@ -56,6 +56,9 @@ export default {
         caution: {
           bg: `buttonWarning.200`,
         },
+        ghost: {
+          bg: `base.transparent`,
+        },
       },
     },
     Toast: {
