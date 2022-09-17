a02c10e82caf6ebd8ca5976f37c03d5be4c9b0ff
Bryson Kruk <brysonmk1984@gmail.com>
2022-09-17 14:36:10 -0600

    latest config, successfully deployed with eas

--- App/Screens/Loading/Loading.tsx
+++ App/Screens/Loading/Loading.tsx
@@ -1,22 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Image, Pressable, FlatList, SectionList, Box, Center, View, Text, Heading, VStack, FormControl, Input, Link, Button, IconButton, HStack, Divider } from "native-base";
-import AnimatedLoader from "react-native-animated-loader";
+import { Box, Center, View, Text, VStack } from "native-base";
+
 import ScreenContainer from "../../Components/ScreenContainer/ScreenContainer";
 import { Dimensions, Platform } from "react-native";
 const Loading = () => {
-  const loaderSize = Dimensions.get("window").width * 1.2;
   const [displayLoadingText, setDisplayLoadingText] = useState(true);
-  //const messages = ["powering up...", "one last squat...", "power walking to the server...", "making sure we are using proper...", '"only five more miles"...', "communing with the elements...", "Doing pull-ups on our database..."];
-  //const [message, setMessage] = useState("powering up...");
-
-  // A loading message that cycles through the messages array above. Only used for initial load.
-  // useEffect(() => {
-  //   const messageInt = setInterval(() => {
-  //     messages.push(messages.shift());
-  //     setMessage(messages[0]);
-  //   }, 3000);
-  //   return () => clearInterval(messageInt);
-  // }, []);
 
   useEffect(() => {
     const loadingInt = setInterval(() => {
@@ -36,9 +24,6 @@ const Loading = () => {
                 Loading...
               </Text>
             </Box>
-            <AnimatedLoader overlayColor="rgba(0,0,0,0)" animationStyle={{ width: loaderSize, height: loaderSize }} visible={true} source={require("./loader.json")} speed={1}>
-              {/* <Text fontFamily="systemFont">{message}</Text> */}
-            </AnimatedLoader>
           </View>
         </VStack>
       </Center>
