3eb3505d96cbb4291e3aaa88f513eb8271fc8eba
Bryson Kruk <brysonmk1984@gmail.com>
2022-09-18 09:40:04 -0600

    updated

--- App/index.js
+++ App/index.js
@@ -4,7 +4,6 @@ import { NativeBaseProvider, extendTheme } from "native-base";
 import { StateProvider } from "./store";
 import App from "./App";
 import herofitTheme from "./styles/herofitTheme";
-// import "react-native-gesture-handler";
 import * as Sentry from "sentry-expo";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
